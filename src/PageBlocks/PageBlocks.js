00331c53083c154b9064043d9de6337f231ac04c
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-21 23:19:48 +0530

    Add a image and apply styles for image

--- src/PageBlocks/PageBlocks.js
+++ src/PageBlocks/PageBlocks.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function PageBlocks() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <div>
+        <img
+          className="image-1"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGFGyTTAAYg5v_oRjooBD46SiO1FWvCt8adbB9ytvm4DZ6JO48QQ"
+          alt="lion"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default PageBlocks;
