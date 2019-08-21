eee34460fed968c5f22e628a2a6aef11c1529aa7
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-21 22:59:10 +0530

    Create Home link

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,6 +5,12 @@ function Header() {
   return (
     <div>
       <div className="title">Pepper Cloud Private Limited</div>
+      <form className="search-button">
+        <input type="text" placeholder="Pepper cloud.." name="search" />
+      </form>
+      <a href="https://medium.com" className="home">
+        Home
+      </a>
     </div>
   );
 }
