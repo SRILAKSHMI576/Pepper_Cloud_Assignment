869976d8e12018b0cfac57e7c7677c05ae0ad5e2
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-22 01:11:23 +0530

    Update Header component into class component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
 import PageBlocks from "../PageBlocks/PageBlocks";
 
-function Header() {
-  return (
-    <div className="header">
-      <div className="title">Pepper Cloud Private Limited</div>
-      <form className="search-button">
-        <input type="text" placeholder="Pepper cloud.." name="search" />
-      </form>
-      <a href="https://medium.com" className="home">
-        Home
-      </a>
-      <PageBlocks />
-    </div>
-  );
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="title">Pepper Cloud Private Limited</div>
+        <form className="search-button">
+          <input type="text" placeholder="Pepper cloud.." name="search" />
+        </form>
+        <a href="https://medium.com" className="home">
+          Home
+        </a>
+        <PageBlocks />
+      </div>
+    );
+  }
 }
 
 export default Header;
