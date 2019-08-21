e2fe3a544a522b8cbc4a9c6cc02add98e6e31567
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-22 00:07:32 +0530

    Add a image and write paragraphs and apply styles for it

--- src/PageBlocks/PageBlocks.js
+++ src/PageBlocks/PageBlocks.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function PageBlocks() {
   return (
-    <div>
+    <div className="main-header">
       <div>
         <img
           className="image-1"
@@ -16,6 +16,16 @@ function PageBlocks() {
         <p className="para-2">Kindness is a superpower</p>
         <p className="para-3">Michael Thompson in Forge</p>
         <p className="para-4">Aug 13. 4 min read </p>
+      </div>
+      <div>
+        <img
+          className="image-2"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmw9BKT1VsMHhp2kcIhkXl6tMsdzJtKCBV-nMoJD3hVEHpu8uJ"
+          alt="nature"
+        />
+        <h3 className="para-5">Stop Making Everything Perfect for Your Kid</h3>
+        <p className="para-6">Michael Thompson in Forge</p>
+        <p className="para-7">Aug 13. 4 min read </p>
       </div>
     </div>
   );
