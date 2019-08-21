54dd03d42def88f7cb50b26f0f8e58eaf5faec97
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-21 22:20:39 +0530

    Create a blog title and apply styles for title

--- src/BlogPages/BlogPages.js
+++ src/BlogPages/BlogPages.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./style.css";
 
 function BlogPages() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <div className="title">Pepper Cloud Private Limited</div>
+    </div>
+  );
 }
 
 export default BlogPages;
