ebf62ca976c239bc1bdc1dc23baa0ab176f6efa7
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-22 15:57:39 +0530

    Add image in a component

--- src/StoryDescription/StoryDescription.js
+++ src/StoryDescription/StoryDescription.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./style.css";
 
 function StoryDescription() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <img
+        className="img"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzbXw0IkEFrVGArPeMnfhwBf74hHj62GoOZtsXvCijS7stjuQsjw"
+      />
+    </div>
+  );
 }
 
 export default StoryDescription;
