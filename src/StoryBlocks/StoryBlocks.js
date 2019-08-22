b37c85862f4e57b05ddc0f8ffb187f0a03f0f0e7
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-22 16:45:45 +0530

    Pass a showDescription function to a component

--- src/StoryBlocks/StoryBlocks.js
+++ src/StoryBlocks/StoryBlocks.js
@@ -1,21 +1,30 @@
 import React, { Component } from "react";
 import "./style.css";
+import { Redirect } from "react-router-dom";
 
 class StoryBlocks extends Component {
   constructor() {
     super();
     this.state = {
-      text: "alert"
+      text: "false"
     };
     this.alertText = this.alertText.bind(this);
   }
 
   alertText(e) {
-    console.log("alert");
+    this.setState({
+      text: true
+    });
+  }
+  showDescription(e) {
+    if (this.state.text) {
+      return <Redirect to="/story-description" />;
+    }
   }
   render() {
     return (
       <div className="main-header">
+        {this.showDescription()}
         <div onClick={this.alertText}>
           <img className="image-1" src={this.props.img} alt="lion" />
           <h3 className="para-1">{this.props.title}</h3>
