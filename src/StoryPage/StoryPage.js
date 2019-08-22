ce170f5e33eea69a29daee93c3e1b9c200fdad42
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-22 11:49:30 +0530

    Create a textarea field and apply styles for it

--- src/StoryPage/StoryPage.js
+++ src/StoryPage/StoryPage.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./style.css";
 
 function StoryPage() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <p className="title-1">Title</p>
+      <textarea className="text-area" placeholder="Write a story..." />
+    </div>
+  );
 }
-
 export default StoryPage;
