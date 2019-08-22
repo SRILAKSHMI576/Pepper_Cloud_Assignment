aaca6743d4c7ab5010c58872b4f68fe679fd5269
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-22 16:18:21 +0530

    Change StoryBlocks into class component

--- src/StoryBlocks/StoryBlocks.js
+++ src/StoryBlocks/StoryBlocks.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
 
-function StoryBlocks(props) {
-  return (
-    <div className="main-header">
-      <div>
-        <img className="image-1" src={props.img} alt="lion" />
-        <h3 className="para-1">{props.title}</h3>
-        <p className="para-2">{props.desciption}</p>
+class StoryBlocks extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "alert"
+    };
+    this.alertText = this.alertText.bind(this);
+  }
+
+  alertText(e) {
+    console.log("alert");
+  }
+  render() {
+    return (
+      <div className="main-header">
+        <div onClick={this.alertText}>
+          <img className="image-1" src={this.props.img} alt="lion" />
+          <h3 className="para-1">{this.props.title}</h3>
+          <p className="para-2">{this.props.desciption}</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default StoryBlocks;
