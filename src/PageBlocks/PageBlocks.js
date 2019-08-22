ff1d47a74a8a86147d18c77c4914934e8b8b372b
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-22 15:15:57 +0530

    place images in flext by using props

--- src/PageBlocks/PageBlocks.js
+++ src/PageBlocks/PageBlocks.js
@@ -1,80 +1,15 @@
 import React from "react";
 import "./style.css";
 
-function PageBlocks() {
+function PageBlocks(props) {
   return (
     <div className="main-header">
       <div>
-        <img
-          className="image-1"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGFGyTTAAYg5v_oRjooBD46SiO1FWvCt8adbB9ytvm4DZ6JO48QQ"
-          alt="lion"
-        />
-        <h3 className="para-1">
-          The Shy Person’s Guide to Winning <br />
-          Friends and Influencing People
-        </h3>
-        <p className="para-2">Kindness is a superpower</p>
-        <p className="para-3">Michael Thompson in Forge</p>
-        <p className="para-4">Aug 13. 4 min read </p>
-      </div>
-      <div className="main-header-1">
-        <div>
-          <div>
-            <img
-              className="image-2"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmw9BKT1VsMHhp2kcIhkXl6tMsdzJtKCBV-nMoJD3hVEHpu8uJ"
-              alt="nature"
-            />
-            <h3 className="para-5">
-              Stop Making Everything Perfect for <br /> Your Kid
-            </h3>
-            <p className="para-6">Michael Thompson in Forge</p>
-            <p className="para-7">Aug 13. 4 min read </p>
-          </div>
-          <div>
-            <img
-              className="image-2"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzVkuGeLO3lPSph8ySVdHOGvUt5Y0eW117DPPlcAVKpSQGUrHkCA"
-              alt="nature"
-            />
-            <h3 className="para-5">
-              What If I Said No? (And Other <br />
-              Questions to Consider Daily)
-            </h3>
-            <p className="para-6">Ryan Holiday in Human Parts</p>
-            <p className="para-7">Aug 21. 12 min read </p>
-          </div>
-          <div>
-            <img
-              className="image-2"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl-vJCUzVa_uQkzVAEFOBX-JJDKVt8S7XzlfCETyrweUz1bO0LoQ"
-              alt="nature"
-            />
-            <h3 className="para-5">
-              How To Keep Going If You Don’t <br />
-              Know What’s Next
-            </h3>
-            <p className="para-6">Darius Foroux in Forge</p>
-            <p className="para-7">Aug 15. 3 min read </p>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div>
-          <img
-            className="image-3"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnBKl8tFuoZaZQMdxz0QblbUn11ATmjuZ6pnE0aC-2PZbyxK9_"
-            alt="nature"
-          />
-          <h3 className="para-8">
-            We Need to Stop Criminalizing <br />
-            School-Age Black Girls
-          </h3>
-          <p className="para-9">What they are, what you need to know, and </p>
-          <p className="para-10">Dana G Smith in Elemental</p>
-          <p className="para-11">Aug 15. 3 min read </p>
-        </div>
+        <img className="image-1" src={props.img} alt="lion" />
+        <h3 className="para-1">{props.heading}</h3>
+        <p className="para-2">{props.heading1}</p>
+        <p className="para-3">{props.heading2}</p>
+        <p className="para-4">{props.heading3}</p>
       </div>
     </div>
   );
--- src/assignmentTask/assignmentTask.js
+++ src/assignmentTask/assignmentTask.js
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
 import Header from "../Header/Header";
 import PageBlocks from "../PageBlocks/PageBlocks";
+import "./style.css";
 
 class AssignmentTask extends Component {
   render() {
     return (
       <div>
         <Header />
-        <PageBlocks />
-        <PageBlocks />
+        <div className="pages">
+          <PageBlocks
+            img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGFGyTTAAYg5v_oRjooBD46SiO1FWvCt8adbB9ytvm4DZ6JO48QQ"
+            heading="The Shy Person’s Guide to Winning 
+          Friends and Influencing People"
+            heading1="Kindness is a superpower"
+            heading2="Michael Thompson in Forge"
+            heading3="Aug 13. 4 min read"
+          />
+          <PageBlocks
+            img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnBKl8tFuoZaZQMdxz0QblbUn11ATmjuZ6pnE0aC-2PZbyxK9_"
+            heading=" We Need to Stop Criminalizing 
+        School-Age Black Girls"
+            heading1="What they are, what you need to know, and"
+            heading2="Dana G Smith in Elemental"
+            heading3="Aug 15. 3 min read"
+          />
+        </div>
+        <div className="pages">
+          <PageBlocks
+            img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmw9BKT1VsMHhp2kcIhkXl6tMsdzJtKCBV-nMoJD3hVEHpu8uJ"
+            heading=" Stop Making Everything Perfect for  Your Kid"
+            heading1="Kindness is a superpower"
+            heading2="Michael Thompson in Forge"
+            heading3="Aug 13. 4 min read"
+          />
+          <PageBlocks
+            img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzVkuGeLO3lPSph8ySVdHOGvUt5Y0eW117DPPlcAVKpSQGUrHkCA"
+            heading=" What If I Said No? (And Other
+              Questions to Consider Daily)"
+            heading1="What they are, what you need to know, and"
+            heading2="Dana G Smith in Elemental"
+            heading3="Aug 15. 3 min read"
+          />
+        </div>
       </div>
     );
   }
