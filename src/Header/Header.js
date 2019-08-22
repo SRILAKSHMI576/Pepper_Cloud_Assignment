70e5c9a0b5cccbcb089170ec90ae5b398d4c519c
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-22 14:11:09 +0530

    Remove search field and styles

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,9 +6,7 @@ class Header extends Component {
     return (
       <div className="header">
         <div className="title">Pepper Cloud Private Limited</div>
-        <form className="search-button">
-          <input type="text" placeholder="Pepper cloud.." name="search" />
-        </form>
+
         <a href="https://medium.com" className="home">
           Home
         </a>
