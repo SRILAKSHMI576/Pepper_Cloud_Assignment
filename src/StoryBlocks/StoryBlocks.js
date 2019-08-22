830d4d442969380d2dadab6c53c82ed9510bfd4c
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-22 15:53:21 +0530

    Remove unnecessary props names

--- src/StoryBlocks/StoryBlocks.js
+++ src/StoryBlocks/StoryBlocks.js
@@ -8,8 +8,6 @@ function StoryBlocks(props) {
         <img className="image-1" src={props.img} alt="lion" />
         <h3 className="para-1">{props.title}</h3>
         <p className="para-2">{props.desciption}</p>
-        <p className="para-3">{props.paragraph}</p>
-        <p className="para-4">{props.para}</p>
       </div>
     </div>
   );
