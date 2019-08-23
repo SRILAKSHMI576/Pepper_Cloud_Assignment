4cf171076856e248557332022b0ea47438595806
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-23 23:22:24 +0530

    Change title while enter text on field

--- src/StoryPage/StoryPage.js
+++ src/StoryPage/StoryPage.js
@@ -2,14 +2,26 @@ import React, { Component } from "react";
 import "./style.css";
 
 class StoryPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "title"
+    };
+  }
+  changeTitle = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
   render() {
     return (
       <div>
         <input
           type="text"
-          placeholder="Title..."
-          value="Title"
+          placeholder="title"
+          value={this.state.title}
           className="title-1"
+          onChange={this.changeTitle}
         />
         <textarea className="text-area" placeholder="Write a story..." />
       </div>
