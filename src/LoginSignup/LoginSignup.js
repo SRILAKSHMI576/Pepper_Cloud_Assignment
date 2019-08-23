7d09804e8173b18e468aa6e58a3cb1731f1f1051
srilakshmi576 <srilakshmigulla576@gmail.com>
2019-08-23 21:45:09 +0530

    Create state values

--- src/LoginSignup/LoginSignup.js
+++ src/LoginSignup/LoginSignup.js
@@ -5,7 +5,9 @@ class LoginSignup extends Component {
   constructor() {
     super();
     this.state = {
-      isLoggedIn: false
+      isLoggedIn: false,
+      username: "name",
+      password: "word"
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -24,11 +26,20 @@ class LoginSignup extends Component {
         <center>
           <p>
             Username:
-            <input type="email" placeholder="Email" required />
+            <input
+              type="email"
+              placeholder="Email"
+              value={this.state.username}
+              required
+            />
           </p>
           <p>
             Password:
-            <input type="password" placeholder="Username" />
+            <input
+              type="password"
+              placeholder="Username"
+              value={this.state.password}
+            />
           </p>
           <button className="submit">Submit</button>
         </center>
